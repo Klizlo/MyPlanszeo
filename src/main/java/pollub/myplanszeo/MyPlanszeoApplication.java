6ecf7a170763c53362fd24ce9b421169cf3bb5e9
Klizlo <klizlo0111@gmail.com>
2024-03-04 14:57:18 +0100

    Init commit

---
+++ src/main/java/pollub/myplanszeo/MyPlanszeoApplication.java
@@ -0,0 +1,13 @@
+package pollub.myplanszeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlanszeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlanszeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pollub/myplanszeo/MyPlanszeoApplicationTests.java
@@ -0,0 +1,13 @@
+package pollub.myplanszeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlanszeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
